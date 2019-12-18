e4035ced8f1a97b4cae81fa2d11ea6f5b0c88bbc
caochunhui369 <1556005467@qq.com>
2019-12-18 10:46:57 +0800

    fitst

---
+++ src/main/java/com/example/cch/demofirstproject/DemoFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cch.demofirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFirstProjectApplication.class, args);
+    }
+
+}
